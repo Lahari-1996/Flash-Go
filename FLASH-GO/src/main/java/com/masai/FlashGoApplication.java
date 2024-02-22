31db72c766f2f3eed8611f9afd56cdf826f45c9f
Lahari-1996 <101380115+Lahari-1996@users.noreply.github.com>
2024-02-23 01:57:10 +0530

    code added

---
+++ FLASH-GO/src/main/java/com/masai/FlashGoApplication.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashGoApplication.class, args);
+	}
+
+}
+
